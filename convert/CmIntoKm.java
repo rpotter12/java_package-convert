837cb088e777cb4b1798f1b8c76358e2115ace34
ROHIT POTTER <34352567+rpotter12@users.noreply.github.com>
2017-12-22 18:27:12 +0530

    Add files via upload

---
+++ convert/CmIntoKm.java
@@ -0,0 +1,10 @@
+package convert;
+public class CmIntoKm
+{
+	public double cmintokm(double a)
+	{
+		double b;
+		b=a/100000;
+		return b;
+	}
+}
---
+++ convert/KmIntoCm.java
@@ -0,0 +1,10 @@
+package convert;
+public class KmIntoCm
+{
+	public double kmintocm(double a)
+	{
+		double b;
+		b=a*100000;
+		return b;
+	}
+}
